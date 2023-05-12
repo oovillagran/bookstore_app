80d3a5b5b967f79904fc4a2ee65bd10ccc4f66fc
Oscar Villagran <oscar.villagran@hotmail.com>
2023-05-12 15:15:04 -0500

    Add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
    <>
+    <Routes>
+      <Route path="/" element={<Booklist />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
    </>
   );
 }
